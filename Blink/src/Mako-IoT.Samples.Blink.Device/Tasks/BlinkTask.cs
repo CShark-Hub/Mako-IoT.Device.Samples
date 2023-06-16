c7d7c07794eb7e81b963baf94adc6e104c5a6863
Bartek U <2597165+jazzman55@users.noreply.github.com>
2023-06-16 11:09:06 +0200

    blink sample (#3)

---
+++ Blink/src/Mako-IoT.Samples.Blink.Device/Tasks/BlinkTask.cs
@@ -0,0 +1,25 @@
+﻿using MakoIoT.Device.Services.Interface;
+using MakoIoT.Samples.Blink.Device.Interface;
+
+namespace MakoIoT.Samples.Blink.Device.Tasks
+{
+    public class BlinkTask : ITask
+    {
+        private readonly IBlinker _blinker;
+        private bool _isOn;
+        
+        public string Id { get; }
+
+        public BlinkTask(IBlinker blinker)
+        {
+            _blinker = blinker;
+            Id = nameof(BlinkTask);
+        }
+
+        public void Execute()
+        {
+            _isOn = !_isOn;
+            _blinker.Blink(_isOn);
+        }
+    }
+}
