ef14747c0f2125986b625b53a792fd656db0401f
Bartek U <2597165+jazzman55@users.noreply.github.com>
2024-02-05 16:02:09 +0100

    move frontend to another folder, bat for gzipping

---
+++ WasteBinsCalendar/src/MakoIoT.Device.LocalConfiguration/CopyFrontendToFileSystem.bat
@@ -0,0 +1,10 @@
+@echo off
+set p7z="C:\Program Files\7-Zip\7z.exe"
+del FileSystem\index.html.gz
+%p7z% a -tgzip "FileSystem\index.html.gz" "Frontend\dist\index.html"
+del FileSystem\index.css.gz
+%p7z% a -tgzip "FileSystem\index.css.gz" "Frontend\dist\index.css"
+del FileSystem\index.js.gz
+%p7z% a -tgzip "FileSystem\index.js.gz" "Frontend\dist\index.js"
+del FileSystem\favicon.ico.gz
+%p7z% a -tgzip "FileSystem\favicon.ico.gz" "Frontend\dist\favicon.ico"
